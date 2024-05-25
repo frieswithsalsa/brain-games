fad2d8d0e38d225d7dde2c3e59dbe8cb128cbf2d
frieswithsalsa <romansedin@hotmail.com>
2024-05-25 18:40:24 +0200

    brain-prime logic

---
+++ games/brainPrimeLogic.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import { greetUser } from '../src/cli.js';
+
+const userName = greetUser();
+
+const isPrime = (n) => {
+    for (let i = 2; i < n; i++) {
+       if (n % i === 0) {
+        return false
+       };
+    }
+    return n > 1;
+   }
+
+export const brainPrime = () => {
+    
+    console.log('Answer "yes" if the number is prime, otherwise answer "no".');
+    let correctAnswersCount = 0;
+    const answersToWin = 3;
+
+    while (correctAnswersCount < answersToWin) {
+        let randomNumber = Math.floor(Math.random() * 101);
+        console.log(`Question: ${randomNumber}`);
+        
+        const userAnswer =  readlineSync.question('Your answer: ');
+
+        const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no'
+
+        if (userAnswer === correctAnswer) {
+            console.log('Correct!');
+            correctAnswersCount += 1;
+        } else {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${userName}`)
+        break
+        }
+    }
+    if (correctAnswersCount === 3) {
+        console.log(`Congratulations, ${userName}!`)
+    }
+}
