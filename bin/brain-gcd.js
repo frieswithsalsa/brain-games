60c3172e3e545acac6f25bf7eb2622b106132cf6
frieswithsalsa <romansedin@hotmail.com>
2024-05-26 16:37:57 +0200

    linter fixes

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { brainGcd } from '../games/brainGcdLogic.js';
+import brainGcd from '../games/brainGcdLogic.js';
 
 brainGcd();
--- games/brainGcdLogic.js
+++ games/brainGcdLogic.js
@@ -4,42 +4,42 @@ import { greetUser } from '../src/cli.js';
 const userName = greetUser();
 
 const findGcd = (a, b) => {
-    while (a != 0 & b != 0) {
-        if (a > b) {
-            a = a % b;
-        } else {
-            b = b % a;
-        }
-    } return a + b;
-}
-
-export const brainGcd = () => {
-    console.log('Find the greatest common divisor of given numbers.');
-    let correctAnswersCount = 0;
-    const answersToWin = 3;
-
-    while (correctAnswersCount < answersToWin) {
-        let randomNumberOne = Math.floor(Math.random() * 101);
-        let randomNumberTwo = Math.floor(Math.random() * 101);
-        
-        console.log(`Question: ${randomNumberOne} ${randomNumberTwo}`);
-        
-        const userAnswer =  readlineSync.question('Your answer: ');
-        
-        let correctAnswer = findGcd(randomNumberOne, randomNumberTwo).toString();
-
-        if (userAnswer === correctAnswer) {
-            console.log('Correct!');
-            correctAnswersCount += 1;
-        } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-        console.log(`Let's try again, ${userName}!`)
-        break
-        } 
-        if (correctAnswersCount === 3) {
-            console.log(`Congratulations, ${userName}!`)
-        }
+  while (a !== 0 & b !== 0) {
+    if (a > b) {
+      a %= b;
+    } else {
+      b %= a;
     }
-
-
+  } return a + b;
 };
+
+const brainGcd = () => {
+  console.log('Find the greatest common divisor of given numbers.');
+  let correctAnswersCount = 0;
+  const answersToWin = 3;
+
+  while (correctAnswersCount < answersToWin) {
+    const randomNumberOne = Math.floor(Math.random() * 101);
+    const randomNumberTwo = Math.floor(Math.random() * 101);
+
+    console.log(`Question: ${randomNumberOne} ${randomNumberTwo}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = findGcd(randomNumberOne, randomNumberTwo).toString();
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
+    if (correctAnswersCount === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+};
+
+export default brainGcd;
