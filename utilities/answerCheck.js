6a32c0ab0617aa7c4cf93dd5c52bd5e08b8b2c29
frieswithsalsa <romansedin@hotmail.com>
2024-05-26 19:09:53 +0200

    refactoring

---
+++ utilities/answerCheck.js
@@ -0,0 +1,15 @@
+const isCorrectAnswer = (userAnswer, correctAnswer, userName, correctAnswersCount) => {
+    if (userAnswer === correctAnswer) {
+        console.log('Correct!');
+        correctAnswersCount += 1;
+      } else {
+        console.log(
+          `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
+        );
+        console.log(`Let's try again, ${userName}!`);
+        return
+      }
+      return correctAnswersCount;
+  };
+
+export default isCorrectAnswer;
