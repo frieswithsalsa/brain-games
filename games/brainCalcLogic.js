e0ddbf0eb09b3b04c11e87d91549757dfcd09285
frieswithsalsa <romansedin@hotmail.com>
2024-05-26 12:53:05 +0200

    lint fixes

--- games/brainCalcLogic.js
+++ games/brainCalcLogic.js
@@ -3,44 +3,46 @@ import { greetUser } from '../src/cli.js';
 
 const userName = greetUser();
 
-export const brainCalc = () => {
-    console.log(`What is the result of the expression?`);
-    let correctAnswersCount = 0;
-    const answersToWin = 3;
+export default brainCalc = () => {
+    console.log('What is the result of the expression?');
+  let correctAnswersCount = 0;
+  const answersToWin = 3;
 
-    while (correctAnswersCount < answersToWin) {
-        let randomNumberOne = Math.floor(Math.random() * 101);
-        let randomNumberTwo = Math.floor(Math.random() * 101);
-        let operationsArray = ['+', '-', '*'];
-        let randomOperation = Math.floor(Math.random() * operationsArray.length);
-        let randomIndex = operationsArray[randomOperation];
+  while (correctAnswersCount < answersToWin) {
+    const randomNumberOne = Math.floor(Math.random() * 101);
+    let randomNumberTwo = Math.floor(Math.random() * 101);
+    const operationsArray = ['+', '-', '*'];
+    const randomOperation = Math.floor(Math.random() * operationsArray.length);
+    let randomIndex = operationsArray[randomOperation];
 
-        console.log(`Question: ${randomNumberOne} ${randomIndex} ${randomNumberTwo}`);
-        const userAnswer =  readlineSync.question('Your answer: ');
-        let correctAnswer;
+    console.log(`Question: ${randomNumberOne} ${randomIndex} ${randomNumberTwo}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    let correctAnswer;
 
-        switch (randomIndex) {
-            case '+':
-            correctAnswer = randomNumberOne + randomNumberTwo;
-            break;
-            case '-':
-            correctAnswer = randomNumberOne - randomNumberTwo;
-            break;
-            case '*':
-            correctAnswer = randomNumberOne * randomNumberTwo;
-            break;
+    switch (randomIndex) {
+        case '+':
+        correctAnswer = randomNumberOne + randomNumberTwo;
+        break;
+        case '-':
+        correctAnswer = randomNumberOne - randomNumberTwo;
+        break;
+        case '*':
+        correctAnswer = randomNumberOne * randomNumberTwo;
+        break;
+        default: 
+        console.log('Error')
         }
 
-        if (userAnswer === correctAnswer.toString()) {
-            console.log('Correct!');
-            correctAnswersCount += 1;
-        } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-        console.log(`Let's try again, ${userName}`)
-        break
-        } 
-        if (correctAnswersCount === 3) {
-            console.log(`Congratulations, ${userName}!`)
-        }
+    if (userAnswer === correctAnswer.toString()) {
+    console.log('Correct!');
+    correctAnswersCount += 1;
+    } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${userName}`);
+    break
+    };
+    if (correctAnswersCount === 3) {
+    console.log(`Congratulations, ${userName}!`);
+        };
     }
 }
