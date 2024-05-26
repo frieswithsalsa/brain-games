a3c89b8cd068230db76d45d12640e2cf47bb9de2
frieswithsalsa <romansedin@hotmail.com>
2024-05-26 16:17:43 +0200

    linter fixes

--- games/brainEvenLogic.js
+++ games/brainEvenLogic.js
@@ -6,28 +6,28 @@ import { greetUser } from '../src/cli.js';
 const userName = greetUser();
 
 const brainEven = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    let correctAnswersCount = 0;
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let correctAnswersCount = 0;
   const answersToWin = 3;
 
   while (correctAnswersCount < answersToWin) {
-  const randomNumber = Math.floor(Math.random() * 101);
-  console.log(`Question: ${randomNumber}`);
-  const userAnswer =  readlineSync.question('Your answer: ');
+    const randomNumber = Math.floor(Math.random() * 101);
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
 
-        if (userAnswer === correctAnswer) {
-            console.log('Correct!');
-            correctAnswersCount += 1;
-        } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-        console.log(`Let's try again, ${userName}!`)
-        break
-        } 
-        if (correctAnswersCount === 3) {
-            console.log(`Congratulations, ${userName}!`)
-        }
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
     }
+    if (correctAnswersCount === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
 };
 
 export default brainEven;
