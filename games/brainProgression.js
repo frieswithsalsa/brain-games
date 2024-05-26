d2ab40b291ea2c1e71f760783168d771845f4783
frieswithsalsa <romansedin@hotmail.com>
2024-05-26 17:25:47 +0200

    linter fixes

--- games/brainProgression.js
+++ games/brainProgression.js
@@ -1,50 +1,51 @@
-import readlineSync from 'readline-sync';
-import { greetUser } from '../src/cli.js';
+import readlineSync from "readline-sync";
+import { greetUser } from "../src/cli.js";
 
 const userName = greetUser();
 
 const randomInt = (min, max) => {
-    const randomNum = min + Math.random() * (max + 1 - min);
-    return Math.floor(randomNum)
-}
+  const randomNum = min + Math.random() * (max + 1 - min);
+  return Math.floor(randomNum);
+};
 
 const progressionLength = 10;
 
 const makeProgression = (num) => {
-    const difference = randomInt(1, 9);
-    return Array(progressionLength)
+  const difference = randomInt(1, 9);
+  return Array(progressionLength)
     .fill(num)
-    .map((elem, index) => elem + index * difference)
-}
+    .map((elem, index) => elem + index * difference);
+};
 
+export const brainProgression = () => {
+  console.log("What number is missing in the progression?");
 
-    export const brainProgression = () => {
-    console.log('What number is missing in the progression?');
-    
-    let correctAnswersCount = 0;
-    const answersToWin = 3;
+  let correctAnswersCount = 0;
+  const answersToWin = 3;
 
-    while (correctAnswersCount < answersToWin) {
-        const startNum = randomInt(1, 100);
-        const progression = makeProgression(startNum);
-        const hiddenIndex = randomInt(0, progressionLength - 1);
-        const correctAnswer = progression[hiddenIndex];
-        progression[hiddenIndex] = '..'
-        const question = progression.join(' ');
+  while (correctAnswersCount < answersToWin) {
+    const startNum = randomInt(1, 100);
+    const progression = makeProgression(startNum);
+    const hiddenIndex = randomInt(0, progressionLength - 1);
+    const correctAnswer = progression[hiddenIndex];
+    progression[hiddenIndex] = "..";
+    const question = progression.join(" ");
 
-        console.log(`Question: ${question}`);
-        const userAnswer = readlineSync.question ('Your answer: ');
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question("Your answer: ");
 
-        if (parseInt(userAnswer) === correctAnswer) {
-            console.log('Correct!');
-            correctAnswersCount += 1;
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-            console.log(`Let's try again, ${userName}!`)
-        break
-        }
+    if (parseInt(userAnswer) === correctAnswer) {
+      console.log("Correct!");
+      correctAnswersCount += 1;
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
+      );
+      console.log(`Let's try again, ${userName}!`);
+      break;
     }
-    if (correctAnswersCount === 3) {
-        console.log(`Congratulations, ${userName}!`)
-    }
-}
+  }
+  if (correctAnswersCount === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
