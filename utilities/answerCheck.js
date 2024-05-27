e21d05c59d0f143b031d3a383033499c2f806c42
frieswithsalsa <romansedin@hotmail.com>
2024-05-27 09:38:38 +0200

    logic refactoring

--- utilities/answerCheck.js
+++ utilities/answerCheck.js
@@ -2,11 +2,10 @@ const isCorrectAnswer = (userAnswer, correctAnswer, userName) => {
   if (userAnswer === correctAnswer.toString()) {
     console.log('Correct!');
     return true;
-  } 
+  }
   console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
   console.log(`Let's try again, ${userName}!`);
   return false;
-  
 };
 
 export default isCorrectAnswer;
