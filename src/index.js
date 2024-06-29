23f8042131dff04fd8237a3ed0a7d584b45e8426
frieswithsalsa <romansedin@hotmail.com>
2024-06-29 12:44:49 +0200

    linter fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const getRandomNumber = () => {
-    const number1 = Math.floor(Math.random() * 101);
-    const number2 = Math.floor(Math.random() * 101);  
-    return {number1, number2}; 
-}
+  const number1 = Math.floor(Math.random() * 101);
+  const number2 = Math.floor(Math.random() * 101);
+  return { number1, number2 };
+};
 
 export default getRandomNumber;
