434e26ea2c74935e017acc0b07667eb3852080a4
frieswithsalsa <romansedin@hotmail.com>
2024-06-29 15:55:23 +0200

    linter

--- src/index.js
+++ src/index.js
@@ -12,9 +12,7 @@ const startGame = (rules, startRound) => {
     console.log(question);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.` );
       console.log(`Let's try again, ${userName}!`);
       return;
     }
@@ -24,3 +22,4 @@ const startGame = (rules, startRound) => {
 };
 
 export default startGame;
+
