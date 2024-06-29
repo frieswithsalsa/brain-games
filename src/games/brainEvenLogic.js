d19d971a069fde67f049159dee17521dfbd8d409
frieswithsalsa <romansedin@hotmail.com>
2024-06-29 15:57:12 +0200

    linter

--- src/games/brainEvenLogic.js
+++ src/games/brainEvenLogic.js
@@ -2,7 +2,7 @@
 import getRandomNumber from '../numbersGenerator.js';
 import startGame from '../index.js';
 
-const rule = `Answer 'yes' if the number is even, otherwise answer 'no'.`;
+const rule = `Answer "yes" if the number is even, otherwise answer "no".`;
 
 const isEven = (number) => number % 2 === 0;
 
