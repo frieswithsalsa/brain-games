4271b3da4c8278c8209a5463d3e4fdf49c045acb
frieswithsalsa <romansedin@hotmail.com>
2024-07-03 16:55:48 +0200

    function fix

--- src/games/brainProgressionLogic.js
+++ src/games/brainProgressionLogic.js
@@ -7,8 +7,8 @@ const rule = 'What number is missing in the progression?';
 const progressionLength = 10;
 
 const makeProgression = (num, difference) => Array(progressionLength)
-    .fill(num)
-    .map((elem, index) => elem + index * difference);
+  .fill(num)
+  .map((elem, index) => elem + index * difference);
 
 const startRound = () => {
   const startNum = getRandomNumber(1, 100);
